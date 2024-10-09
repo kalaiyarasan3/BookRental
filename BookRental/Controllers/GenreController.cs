c24d875c50d2e16fa44ea9d845ef15c03eca61d9
kalaiyarasan3 <kalai243526@gmail.com>
2024-10-10 02:55:41 +0530

    Create Genre Controller and View

---
+++ BookRental/Controllers/GenreController.cs
@@ -0,0 +1,46 @@
+﻿using BookRental.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BookRental.Controllers
+{
+    public class GenreController : Controller
+    {
+
+        private readonly ApplicationDbContext _context;
+        public GenreController()
+        {
+            _context=new ApplicationDbContext();
+        }
+        // GET: Genre
+        public ActionResult Index()
+        {
+            var genre = _context.Genres.ToList();
+            return View(genre);
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Genre genre)
+        {
+            if(ModelState.IsValid)
+            {
+                _context.Genres.Add(genre);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+		protected override void Dispose(bool disposing)
+		{
+			_context.Dispose();
+		}
+	}
+}
