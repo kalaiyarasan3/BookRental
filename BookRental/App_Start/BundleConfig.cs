e00c7e25dc57aed796047ed947554771c4bf23bd
kalaiyarasan3 <kalai243526@gmail.com>
2024-10-10 01:17:23 +0530

    Admin Menu

--- BookRental/App_Start/BundleConfig.cs
+++ BookRental/App_Start/BundleConfig.cs
@@ -20,7 +20,8 @@ namespace BookRental
 						"~/Scripts/modernizr-*"));
 
 			bundles.Add(new Bundle("~/bundles/bootstrap").Include(
-					  "~/Scripts/bootstrap.js"));
+					  "~/Scripts/bootstrap.js",
+					  "~/Scripts/AdminMenu.js"));
 
 			bundles.Add(new StyleBundle("~/Content/css").Include(
 					  "~/Content/bootstrap.css",
---
+++ BookRental/Scripts/AdminMenu.js
@@ -0,0 +1,10 @@
+﻿    $(function () {
+        $('.dropdown').hover(
+            function () {
+                $(this).find('.dropdown-menu').stop(true, true).slideDown(200);
+            },
+            function () {
+                $(this).find('.dropdown-menu').stop(true, true).slideUp(200);
+            }
+        );
+  });
