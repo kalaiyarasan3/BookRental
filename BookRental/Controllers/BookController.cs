06550fbda66262d10e1853e6e7f555394c9135a7
kalaiyarasan3 <kalai243526@gmail.com>
2024-10-11 03:00:45 +0530

    Add Book Controller and Index View

---
+++ BookRental/Controllers/BookController.cs
@@ -0,0 +1,30 @@
+﻿using BookRental.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BookRental.Controllers
+{
+	public class BookController : Controller
+	{
+		private readonly ApplicationDbContext _contect;
+		public BookController()
+		{
+			_contect = new ApplicationDbContext();
+		}
+		// GET: Book
+		public ActionResult Index()
+		{
+			var bookInDb = _contect.Books.Include(x => x.Genre).ToList();
+			return View(bookInDb);
+		}
+
+		protected override void Dispose(bool disposing)
+		{
+			_contect.Dispose();
+		}
+	}
+}
