0fe700704bb84cbd63edc67507c999afc695d79f
kalaiyarasan3 <kalai243526@gmail.com>
2024-10-11 15:30:24 +0530

    Add Book Details And Edit

--- BookRental/Controllers/BookController.cs
+++ BookRental/Controllers/BookController.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using BookRental.ViewModel;
+using System.Net;
 namespace BookRental.Controllers
 {
 	public class BookController : Controller
@@ -26,7 +27,7 @@ namespace BookRental.Controllers
 			var genreInDb = _contect.Genres.ToList();
 			var model = new BookViewModel
 			{
-				Genre= genreInDb
+				Genre = genreInDb
 			};
 			return View(model);
 		}
@@ -34,7 +35,7 @@ namespace BookRental.Controllers
 		[ValidateAntiForgeryToken]
 		public ActionResult Create(BookViewModel viewModel)
 		{
-			if(!ModelState.IsValid)
+			if (!ModelState.IsValid)
 			{
 				var genreInDb = _contect.Genres.ToList();
 				viewModel.Genre = genreInDb;
@@ -42,24 +43,93 @@ namespace BookRental.Controllers
 			}
 			var book = new Book
 			{
-				Author=viewModel.Book.Author,
-				Avaibility=viewModel.Book.Avaibility,
-				DateAdded=viewModel.Book.DateAdded,
-				Description=viewModel.Book.Description,
-				Genre=viewModel.Book.Genre,
-				GenreId=viewModel.Book.GenreId,
-				ImageUrl=viewModel.Book.ImageUrl,
-				ISBN=viewModel.Book.ISBN,
-				Pages=viewModel.Book.Pages,
-				Price=viewModel.Book.Price,
-				ProductDimensions=viewModel.Book.ProductDimensions,
-				PublicationDate=viewModel.Book.PublicationDate,
-				Publisher=viewModel.Book.Publisher,
-				Title=viewModel.Book.Title
+				Author = viewModel.Book.Author,
+				Avaibility = viewModel.Book.Avaibility,
+				DateAdded = viewModel.Book.DateAdded,
+				Description = viewModel.Book.Description,
+				Genre = viewModel.Book.Genre,
+				GenreId = viewModel.Book.GenreId,
+				ImageUrl = viewModel.Book.ImageUrl,
+				ISBN = viewModel.Book.ISBN,
+				Pages = viewModel.Book.Pages,
+				Price = viewModel.Book.Price,
+				ProductDimensions = viewModel.Book.ProductDimensions,
+				PublicationDate = viewModel.Book.PublicationDate,
+				Publisher = viewModel.Book.Publisher,
+				Title = viewModel.Book.Title
 			};
 			_contect.Books.Add(book);
 			_contect.SaveChanges();
 
+			return RedirectToAction("Index");
+		}
+
+		public ActionResult Details(int? id)
+		{
+			if (id == null)
+			{
+				return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+			}
+			var bookInDb = _contect.Books.Find(id);
+			if (bookInDb == null)
+			{
+				return HttpNotFound();
+			}
+			var model = new BookViewModel
+			{
+				Book = bookInDb,
+				Genre = _contect.Genres.ToList()
+			};
+			return View(model);
+		}
+
+		public ActionResult Edit(int? id)
+		{
+			if (id == null)
+			{
+				return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+			}
+			var bookInDb = _contect.Books.Find(id);
+			if (bookInDb == null)
+			{
+				return HttpNotFound();
+			}
+			var model = new BookViewModel
+			{
+				Book = bookInDb,
+				Genre = _contect.Genres.ToList()
+			};
+			return View(model);
+		}
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public ActionResult Edit(BookViewModel viewModel)
+		{
+			if (!ModelState.IsValid)
+			{
+				viewModel.Genre = _contect.Genres.ToList();
+				return View(viewModel);
+			}
+			var book = new Book
+			{
+				Id = viewModel.Book.Id,
+				Author = viewModel.Book.Author,
+				Avaibility = viewModel.Book.Avaibility,
+				DateAdded = viewModel.Book.DateAdded,
+				Description = viewModel.Book.Description,
+				Genre = viewModel.Book.Genre,
+				GenreId = viewModel.Book.GenreId,
+				ImageUrl = viewModel.Book.ImageUrl,
+				ISBN = viewModel.Book.ISBN,
+				Pages = viewModel.Book.Pages,
+				Price = viewModel.Book.Price,
+				ProductDimensions = viewModel.Book.ProductDimensions,
+				PublicationDate = viewModel.Book.PublicationDate,
+				Publisher = viewModel.Book.Publisher,
+				Title = viewModel.Book.Title
+			};
+			_contect.Entry(book).State = EntityState.Modified;
+			_contect.SaveChanges();
 			return RedirectToAction("Index");
 		}
 		protected override void Dispose(bool disposing)
