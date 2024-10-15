ab3bc20760459ac8ca384b57f45c70f66e5d0fca
kalaiyarasan3 <kalai243526@gmail.com>
2024-10-16 01:59:56 +0530

    User Controller Edit,Details and Delete Funtions and Views Added

--- BookRental/Controllers/UserController.cs
+++ BookRental/Controllers/UserController.cs
@@ -5,6 +5,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Net;
+using Microsoft.Ajax.Utilities;
 
 namespace BookRental.Controllers
 {
@@ -35,6 +37,130 @@ namespace BookRental.Controllers
             return View(userList);
         }
 
+		public ActionResult Details(string id)
+		{
+			if (id == null || id.Length == 0)
+			{
+				return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+			}
+			ApplicationUser user = _context.Users.Find(id);
+			UserViewModel model = new UserViewModel()
+			{
+				FirstName = user.FirstName,
+				LastName = user.LastName,
+				Email = user.Email,
+				BirthDate = user.BirthDate,
+				Id = user.Id,
+				MembershipTypeId = user.MembershipId,
+				MembershipTypes = _context.membershipTypes.ToList(),
+				Phone = user.Phone,
+				Disabled = user.Disable
+			};
+			return View(model);
+		}
+
+		public ActionResult Edit(string id)
+		{
+			if (id == null)
+			{
+				return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+			}
+			ApplicationUser user = _context.Users.Find(id);
+
+			if (user == null)
+			{
+				return HttpNotFound();
+			}
+			UserViewModel model = new UserViewModel()
+			{
+				FirstName = user.FirstName,
+				LastName = user.LastName,
+				Email = user.Email,
+				BirthDate = user.BirthDate,
+				Id = user.Id,
+				MembershipTypeId = user.MembershipId,
+				MembershipTypes = _context.membershipTypes.ToList(),
+				Phone = user.Phone,
+				Disabled = user.Disable
+			};
+
+			return View(model);
+		}
+
+
+		//POST Method for EDIT Action
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public ActionResult Edit(UserViewModel user)
+		{
+			if (!ModelState.IsValid)
+			{
+				UserViewModel model = new UserViewModel()
+				{
+					FirstName = user.FirstName,
+					LastName = user.LastName,
+					Email = user.Email,
+					BirthDate = user.BirthDate,
+					Id = user.Id,
+					MembershipTypeId = user.MembershipTypeId,
+					MembershipTypes = _context.membershipTypes.ToList(),
+					Phone = user.Phone,
+					Disabled = user.Disabled
+				};
+				return View(model);
+			}
+			else
+			{
+				var userInDb = _context.Users.Single(u => u.Id == user.Id);
+				userInDb.FirstName = user.FirstName;
+				userInDb.LastName = user.LastName;
+				userInDb.BirthDate = user.BirthDate;
+				userInDb.Email = user.Email;
+				userInDb.MembershipId = user.MembershipTypeId;
+				userInDb.Phone = user.Phone;
+				userInDb.Disable = user.Disabled;
+			}
+
+			_context.SaveChanges();
+
+			return RedirectToAction("Index", "User");
+		}
+		public ActionResult Delete(string id)
+		{
+			if (id == null || id.Length == 0)
+			{
+				return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+			}
+			ApplicationUser user = _context.Users.Find(id);
+			UserViewModel model = new UserViewModel()
+			{
+				FirstName = user.FirstName,
+				LastName = user.LastName,
+				Email = user.Email,
+				BirthDate = user.BirthDate,
+				Id = user.Id,
+				MembershipTypeId = user.MembershipId,
+				MembershipTypes = _context.membershipTypes.ToList(),
+				Phone = user.Phone,
+				Disabled = user.Disable
+			};
+			return View(model);
+		}
+
+		[HttpPost, ActionName("Delete")]
+		[ValidateAntiForgeryToken]
+		public ActionResult UsersDeletePost(string id)
+		{
+			var userInDb = _context.Users.Find(id);
+			if (id == null || id.Length == 0)
+			{
+				return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+			}
+
+			userInDb.Disable = true;
+			_context.SaveChanges();
+			return RedirectToAction("Index", "User");
+		}
 		protected override void Dispose(bool disposing)
 		{
 			_context.Dispose();
