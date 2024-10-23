520f2feba3e65a099287e32f09cb8b66245ac042
kalaiyarasan3 <kalai243526@gmail.com>
2024-10-23 16:08:34 +0530

    Created Book And Users Api

---
+++ BookRental/Controllers/Api/UsersApiController.cs
@@ -0,0 +1,43 @@
+﻿using BookRental.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace BookRental.Controllers.Api
+{
+    public class UsersApiController : ApiController
+    {
+        public ApplicationDbContext _context;
+        public UsersApiController() {
+            _context = new ApplicationDbContext();
+        }
+
+        public IHttpActionResult Get(string type,string query=null)
+        {
+            if(type.Equals("email")&&query !=null)
+                    {
+				var customerQuery = _context.Users.Where(u => u.Email.ToLower().Contains(query.ToLower()));
+
+				return Ok(customerQuery.ToList());
+            }
+            if(type.Equals("name")&& query !=null)
+            {
+                var customerQuery = from u in _context.Users
+                                    where u.Email.ToLower().Contains(query)
+                                    select new { u.FirstName, u.LastName, u.BirthDate };
+
+                return Ok(customerQuery.ToList()[0].FirstName+" " + customerQuery.ToList()[0].FirstName + ";" + customerQuery.ToList()[0].BirthDate);
+
+            }
+            return Ok();
+        }
+
+		protected override void Dispose(bool disposing)
+		{
+			_context.Dispose();
+		}
+	}
+}
